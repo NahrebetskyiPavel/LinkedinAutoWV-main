c9a294d4a4f7670d719af9e833f21bb114f3be13
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2023-11-08 14:43:35 +0200

    data

--- src/test/java/com/automation/linkedin/pages/mynetwork/Connections.java
+++ src/test/java/com/automation/linkedin/pages/mynetwork/Connections.java
@@ -1,5 +1,12 @@
 package com.automation.linkedin.pages.mynetwork;
 
-public class Connections {
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class Connections {
+ElementsCollection MessageBtn = $$x("//div[@class='entry-point']/button");
+SelenideElement messageOverlayConversationBubbleItem = $x("//div[@data-view-name='message-overlay-conversation-bubble-item']");
 }
