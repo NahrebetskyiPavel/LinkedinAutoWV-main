7911b0924ff46c25f191adcfdce11396a5e8129d
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2023-12-20 14:44:06 +0200

    remove

--- src/test/java/com/automation/linkedin/AddLeads.java
+++ src/test/java/com/automation/linkedin/AddLeads.java
@@ -80,127 +80,12 @@ public class AddLeads extends Base {
     public static Object[][] dataProviderPeopleSearch() {
 
         return new Object[][]{
-                {       "Настя ",
-                        "anastasiiakuntii@gmail.com",
-                        "33222200Shin",
-                        "Hi. I came across your account and found that we have some common interests. Would you like to chat a little about the Australian market and some new tendencies and opportunities within it? ;)",
-                        "Anastasiia K."
-                },
-                {       "Pasha",
-                        "pavelnagrebetski@gmail.com",
-                        "Asd321qq",
-                        "Hi. I stumbled upon your account and noticed that you have expertise in my area of interest. I was wondering if you would mind having a chat about the real estate market in the US, its challenges and opportunities ;)",
-                        "Pavel"
-                },
-                {       "Александра",
-                        "alexandra.sternenko@gmail.com",
-                        "asd321qq",
-                        "Hello there. I stumbled across your account accidentally and was impressed with your expertise. Would you mind accepting this invite so we could talk some more?",
-                        "Aleksandra"
-                },
-                {       "Маша",
-                        "deynekamariawv@gmail.com",
-                        "qwertqaz1234",
-                        "Hello there. I stumbled across your account accidentally and was impressed with your expertise. Would you mind accepting this invite so we could talk some more?",
-                        "Mariia"
-                },
-                {       "Михайло",
-                        "michael.salo1995@gmail.com",
-                        "newman1996",
-                        "Hello there. I happened upon your account accidentally and was impressed with your expertise. How about accepting this invite so that we can talk some more?",
-                        "Michael"
-                },
-/*                {       "Nikita - Stockholm board of directors",
-                        clientName,
+                {       "Nikita ",
                         "kni2012@ukr.net",
                         "33222200s",
-                        "https://www.linkedin.com/search/results/people/?geoUrn=%5B%22100907646%22%5D&network=%5B%22O%22%5D&origin=FACETED_SEARCH&page=50&sid=E7r&titleFreeText=board%20of%20directors",
-                        "Hello there. I stumbled across your account by chance and was impressed with your expertise. Would you mind accepting this invite to have an opportunity to talk in the future?",
+                        "Hi. I came across your account and found that we have some common interests. Would you like to chat a little about the Australian market and some new tendencies and opportunities within it? ;)",
                         "Valeriia",
-                        "Stockholm November",
-                        "421659000009767013",
-                        true
-                },*/
-                {       "Наталья- Gothenburg CEO",
-                        "natalia.marcoon@gmail.com ",
-                        "33222200Shin",
-                        "Hello there. I stumbled across your account accidentally and was impressed with your expertise. Would you mind accepting this invite so we could talk some more?",
-                        "Natalia"
-                },
-                {       "Денис - Stockholm CEO",
-                        "basdenisphytontm@gmail.com",
-                        "33222200Shin_",
-                        "Hello there. I stumbled across your account by chance and was impressed with your expertise. Would you mind accepting this invite to have an opportunity to talk in the future?",
-                        "Denis"
-                },
-
-                {       "Роксолана - Stockholm CFO",
-                        "roksolanatrofim@gmail.com ",
-                        "89fcmTT88V",
-                        "Hello there. I stumbled across your account accidentally and was impressed with your expertise. Would you mind accepting this invite so we could talk some more?",
-                        "Roksolana"
-                },
-                {       "Марьян -  Stockholm CTO",
-                        "reshetunmaryanwv@gmail.com",
-                        "rSbnGaRS",
-                        "Hello there. I stumbled across your account accidentally and was impressed with your expertise. Would you mind accepting this invite so we could talk some more?",
-                        "Maryan"
-                },
-
-                {       "Анастасия - Stockholm owner",
-                        "vozniakanastasia52@gmail.com",
-                        "33222200Shin",
-                        "Hi. I stumbled upon your account and noticed that you have expertise in my area of interest. I was wondering if you would mind having a chat about the real estate market in the US, its challenges and opportunities ;)",
-                        "Anastasiia"
                 }
-
-/* ==================================================================================================================================================================== */
-/*                {       "Максим - Stockholm co-founder",
-                        clientName,
-                        "kotokmaksym@gmail.com",
-                        "r4E3w2q1",
-                        "https://www.linkedin.com/search/results/people/?geoUrn=%5B%22100907646%22%5D&network=%5B%22O%22%5D&origin=FACETED_SEARCH&page=80&sid=SEz&titleFreeText=co-founder",
-                        "Hello there. I stumbled across your account accidentally and was impressed with your expertise. Would you mind accepting this invite so we could talk some more?",
-                        "Yurij",
-                        "Stockholm November",
-                        "421659000009767013",
-                        false
-                },*/
-/*                {       "Софія",
-                        clientName,
-                        "sofi.podlesna@gmail.com",
-                        "7riuwotu949",
-                        "https://www.linkedin.com/search/results/people/?geoUrn=%5B%22100459316%22%5D&origin=FACETED_SEARCH&page=85&sid=B%2Cg&titleFreeText=president",
-                        "Hi, NAME. I came across your account and was impressed with your expertise. Would you mind having a quick chat about the Saudi Arabian market and the opportunities professional web development offers to businesses overall and your company in particular? \n",
-                        "Alex",
-                        "Saudi Arabia",
-                        "421659000006238011"
-                },
-                {       "Test",
-                        clientName,
-                        "wisevision1985@gmail.com",
-                        "33222200Shin",
-                        "https://www.linkedin.com/search/results/people/?geoUrn=%5B%22103644278%22%5D&network=%5B%22O%22%5D&origin=FACETED_SEARCH&page=100&sid=Ng9",
-                        "Hi, I want expand my contact, thx for accepting",
-                        "Pavlo",
-                        "Test",
-                        "421659000007355929"
-                },
-                {       "Test",
-                        clientName,
-                        "wisevision1986@gmail.com",
-                        "33222200Shin",
-                        "https://www.linkedin.com/search/results/people/?geoUrn=%5B%22103644278%22%5D&network=%5B%22O%22%5D&origin=FACETED_SEARCH&page=90&sid=Ng9",
-                        "Hi, I want expand my contact, thx for accepting",
-                        "Pavlo",
-                        "Test",
-                        "421659000007355929"
-                },
-<==============================================================================================================================================================>
-
-
-
-               */
         };
     }
 }
