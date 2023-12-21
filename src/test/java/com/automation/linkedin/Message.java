db4caecfe2cf88ca8f30a18486294615e2d9aa58
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2023-12-21 18:37:08 +0200

    asfa

--- src/test/java/com/automation/linkedin/Message.java
+++ src/test/java/com/automation/linkedin/Message.java
@@ -313,10 +313,10 @@ public class Message extends Base{
     public static Object[][] dataProviderPeopleSearch() {
         return new Object[][]{
 
-                {       "Анастасия",
-                        "vozniakanastasia52@gmail.com",
+                {       "Настя - Stuttgart CEO",
+                        "anastasiiakuntii@gmail.com",
                         "33222200Shin",
-                        "Valeriia",
+                        "Alex",
                 }
         };
     }
