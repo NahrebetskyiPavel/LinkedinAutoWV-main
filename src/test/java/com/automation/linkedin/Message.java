46f95ceac7dc281aa789c80e1770fd5e69389dd7
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2023-12-27 14:21:06 +0200

    data

--- src/test/java/com/automation/linkedin/Message.java
+++ src/test/java/com/automation/linkedin/Message.java
@@ -313,7 +313,7 @@ public class Message extends Base{
     public static Object[][] dataProviderPeopleSearch() {
         return new Object[][]{
 
-                {       "natalia",
+                {       "Natalia",
                         "natalia.marcoon@gmail.com",
                         "33222200Shin",
                         "Valeriia",
