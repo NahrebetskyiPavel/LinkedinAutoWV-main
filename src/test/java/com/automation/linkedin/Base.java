fc48b5cce7379288137d59d9f6b5a1485e05922a
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2023-12-28 13:56:12 +0200

    change signin logic

--- src/test/java/com/automation/linkedin/Base.java
+++ src/test/java/com/automation/linkedin/Base.java
@@ -50,7 +50,9 @@ public class Base {
     @SneakyThrows
     public static void openLinkedInLoginPage(){
         Selenide.open("https://www.linkedin.com/login");
+        Thread.sleep(10000);
         WebDriverRunner.getWebDriver().quit();
+        Thread.sleep(10000);
         Selenide.open("https://www.linkedin.com/login");
     }
 
