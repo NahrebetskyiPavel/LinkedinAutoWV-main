25e10521cbf82f2c89d7de2c3eca87d3d4a87c80
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2023-12-29 14:59:48 +0200

    attemptToContactStatusid

--- src/test/java/com/automation/linkedin/AddLeads.java
+++ src/test/java/com/automation/linkedin/AddLeads.java
@@ -28,14 +28,15 @@ public class AddLeads extends Base {
     int low = 2000;
     int high = 5000;
     int randomResult = random.nextInt(high-low) + low;
+    String attemptToContactStatusid = "421659000010541270";
 
     @SneakyThrows
     @Test(description = "add leads from search page", dataProvider = "dataProviderPeopleSearch", alwaysRun = true )
     public void addLeads(String name, String email, String password,  String msg, String linkedinperson){
         int leadsRequestCount = 0;
         Thread.sleep(randomResult);
-        //String token = zohoCrmHelper.renewAccessToken();
-        String token = "1000.d5c088f9eb0c213f66c42e87ff3cdcc3.c63bf8adb088e205e793ac2a3c82a1a4";
+        String token = zohoCrmHelper.renewAccessToken();
+        //String token = "1000.d5c088f9eb0c213f66c42e87ff3cdcc3.c63bf8adb088e205e793ac2a3c82a1a4";
         String data = zohoCrmHelper.getLeadList( token, 1,  "Waiting",  linkedinperson);
         if (data.isEmpty()) {
             System.out.println("Skip" + linkedinperson);
@@ -66,7 +67,7 @@ public class AddLeads extends Base {
                 String id = new JSONObject( data ).getJSONArray("data").getJSONObject(i).getString("id");
                 personPage.addToFriends(msg,false);
             {
-                String changeLeadStatusResponse = zohoCrmHelper.changeLeadStatus(id, token, "421659000001302365");
+                String changeLeadStatusResponse = zohoCrmHelper.changeLeadStatus(id, token, attemptToContactStatusid);
                 JSONObject changeLeadStatusResponseJson = new JSONObject(changeLeadStatusResponse);;
                 System.out.println("code: " + changeLeadStatusResponseJson.getString("code") );
                 System.out.println("\n" );
@@ -81,7 +82,7 @@ public class AddLeads extends Base {
 
         return new Object[][]{
 
-                {       "Анастасия - Stockholm owner",
+                {       "Anastasiia",
                         "vozniakanastasia52@gmail.com",
                         "33222200Shin",
                         "Hi. I stumbled upon your account and noticed that you have expertise in my area of interest. I was wondering if you would mind having a chat about the real estate market in the US, its challenges and opportunities ;)",
