007128e0a33c86eec0799f07ab9976b12a053920
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2023-12-29 14:44:33 +0200

    attemptToContactStatusid

--- src/test/java/com/automation/linkedin/AddLeads.java
+++ src/test/java/com/automation/linkedin/AddLeads.java
@@ -80,8 +80,8 @@ public class AddLeads extends Base {
 
         return new Object[][]{
                 {       "Denis",
-                        "basdenisphytontm@gmail.com",
-                        "33222200Shin",
+                        "denysbasbas@gmail.com",
+                        "asd321qq",
                         "Hello there. I stumbled across your account by chance and was impressed with your expertise. Would you mind accepting this invite to have an opportunity to talk in the future?",
                         "Denis"
                 }
