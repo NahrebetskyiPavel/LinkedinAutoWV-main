9856a588b3085ea77683014bee0e07cf6f315c4b
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2023-12-29 13:35:36 +0200

    change pass

--- src/main/java/api/helpers/ZohoCrmHelper.java
+++ src/main/java/api/helpers/ZohoCrmHelper.java
@@ -430,8 +430,8 @@ if (tasksData.getJSONArray("data").length() >0){
     @Test
     public void getToken1(){
         String token = this.renewAccessToken();
-      //  System.out.println(this.changeTaskStatus(token, "421659000009278012","Closed"));
+      System.out.println(this.changeLeadStatus("421659000011061101", token, "421659000010541270"));
 
-        System.out.println(        this.getLeadList( "1000.e55641d1c24325701b254720c4a027d4.e4ae5ca1fe748b8b35963200e33b9468", 0,  "Waiting",  "Anastasiia") );
+     //   System.out.println(        this.getLeadList( "1000.e55641d1c24325701b254720c4a027d4.e4ae5ca1fe748b8b35963200e33b9468", 0,  "Waiting",  "Anastasiia") );
     }
 }
