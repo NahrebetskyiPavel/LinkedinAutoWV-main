164bda412ef6e0f0d4f47a6d1f845a9cc7f8d841
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2024-01-02 10:45:12 +0200

    name

--- src/test/java/com/automation/linkedin/Base.java
+++ src/test/java/com/automation/linkedin/Base.java
@@ -48,8 +48,6 @@ public class Base {
     }
     public static void openLinkedInLoginPage(){
         Selenide.open("https://www.linkedin.com/login");
-        WebDriverRunner.getWebDriver().quit();
-        Selenide.open("https://www.linkedin.com/login");
     }
 
     public static void closeMsgPopups(){
