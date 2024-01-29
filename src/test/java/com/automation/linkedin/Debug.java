0a5942275a05fa070a0ce049970403a1b82fffd0
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2024-01-29 13:30:39 +0200

    params

--- src/test/java/com/automation/linkedin/Debug.java
+++ src/test/java/com/automation/linkedin/Debug.java
@@ -2,9 +2,13 @@ package com.automation.linkedin;
 
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class Debug {
     @Test
     public void test(){
-        System.getenv().forEach((k, v) -> System.out.println(k + ":" + v));
+        Map<String, String> env = System.getenv();
+        System.out.println(env.get("LOGIN"));
+        System.out.println(env.get("PASSWORD"));
     }
 }
