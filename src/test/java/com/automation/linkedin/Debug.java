41dbe106aecc3726803255c1192999c7f44463f1
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2024-01-29 13:26:06 +0200

    params

--- src/test/java/com/automation/linkedin/Debug.java
+++ src/test/java/com/automation/linkedin/Debug.java
@@ -5,8 +5,6 @@ import org.testng.annotations.Test;
 public class Debug {
     @Test
     public void test(){
-        System.getenv().forEach((k, v) -> {
-            System.out.println(k + ":" + v);
-        }););
+        System.getenv().forEach((k, v) -> System.out.println(k + ":" + v));
     }
 }
