b16deb607a55e68a92f9e93d477f6fcf87bd8ffe
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2024-04-23 10:44:22 +0300

    fix err

--- src/test/java/com/automation/linkedin/AddLeads.java
+++ src/test/java/com/automation/linkedin/AddLeads.java
@@ -150,19 +150,19 @@ public class AddLeads extends Base {
                         "Andrei Gorbunkov"
                 },*/
 
-/*                {       "paul-bereza",
+                {       "paul-bereza",
                         "paul.bereza02@outlook.de",
                         "33222200Shin",
                         "AQEDAUt7kjAFQ5V1AAABjY17BIQAAAGOrZocyk0ADu01WzC7kYz9BmnkvrCJtPNphBAckembhcTOWFgLSCn2xdhVluaqk3DacYtyQsrIBgd8c7_NtYwfevHyEfAhOtIcLkxdxfUEgj04dDbx_uSrdTPY",
                         "Paul Bereza"
-                },*/
+                },
                 {       "alessio-vacenko-b506612b3",
                         "alessio.Vacenko@outlook.it",
                         "33222200Shin",
                         "AQEDAUthqywBUW83AAABjoGzPasAAAGO0M-Kik0Ae4BcJ1UTuha87SZJeYtjcP_2EgzmyJOQ2w90rDaR6EKdLlcwxVl_hIYUBgy3IGj22i-BN7hoWrqeZGKPi_iW1-CDfDDZAMgu73Ob4spxYIZu68uP",
                         "Alessio Vacenko"
                 },
-/*                {       "margit-matthes",
+                {       "margit-matthes",
                         "margit.Matthes@outlook.de",
                         "33222200Shin",
                         "AQEDAUuampkCi39pAAABjoGopG0AAAGO0NAIJE0ANY67cipBXpm84lAAO5KsSsnGqeB1OK-HxQ37rLF4cr5j7qraGxRj9dY1e47pYWWgeevakyzo_wrohxtHSrP6PDsCQOq7LBb5f0FxITaAZLFCP--b",
@@ -173,7 +173,7 @@ public class AddLeads extends Base {
                         "33222200Shin",
                         "AQEDAUsszhwEAKkFAAABjWPddXUAAAGO5z0PXE0ARMhQR0-MOEBRh7EyBGRM-S4jmoyUlHfdOlcjgh6xVE_e2dvxPYueFIGTfc1OUqDu-sfytCMrY7SpLdhYUKsjMrrlIjul4LmJu2_DWUNg5JkJ4usr",
                         "Dmitriy Timashov"
-                },*/
+                },
 
         };
     }
