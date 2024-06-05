802b324e0ece8b51df217949ed55f4667cafe91f
Hryhorii Opanasenko <pnagrebetskyi@gmail.com>
2024-06-05 17:58:22 +0300

    cookie

--- src/test/java/com/automation/crm/ChangeLead.java
+++ src/test/java/com/automation/crm/ChangeLead.java
@@ -43,6 +43,7 @@ public class ChangeLead {
         System.out.println("cookie " + cookie);
         System.out.println("linkedinperson " + linkedinperson);
         String connectionsListTask = zohoCrmHelper.getConnectionsList(profileId, email, password, cookie, "Recently added", numberOfProfiles);
+        Thread.sleep(1000*60);
         String connectionsListTaskId = String.valueOf(new JSONObject( connectionsListTask ).get("taskId"));
         System.out.println(connectionsListTaskId);
         Thread.sleep(1000*60);
@@ -98,44 +99,48 @@ public class ChangeLead {
     public static Object[][] dataProviderPeopleAddToCRM() {
         return new Object[][]{
 
-/*                {       "andrei-gorbunkov-a34b4a2aa",
+                {       "margit-matthes",
+                        "margit.Matthes@outlook.de",
+                        "33222200Shin",
+                        "AQEDAUuampkDA9uSAAABjzNCPogAAAGPV07CiFYAUTlaawCUQBq15anEMNwZmKlCqaTK2oqUSr-P05fihZvoGHPgFk7KZroX9ZKpVEbvEjOLth1xcqkJDG3F_a2_o_3WYwet4mooFR5SjuTy2Z_eezlj",
+                        "Margit Matthes"
+                },
+                {       "andrei-gorbunkov-a34b4a2aa",
                         "andreiGorbunkov@outlook.de",
                         "33222200Shin",
-                        "AQEDAUqQcUgAJO_LAAABjRGv3SIAAAGOmMFYqE0ArnVnmtRxkfVOu6vUysML6PHk2oENpaWG43H6H_RZGisvCqLeBj7azZTBPn0_vjE7zPme8YjHw6GyXwEOBkQvUkqNijYnP9HnwG2A5y5wR9E-hY_q",
+                        "AQEDAUqQcUgEIxsAAAABjvWFnKYAAAGPlUYADFYAombm43_GJ7Tg5JgPeG6gMA7igoCuX850p-7SUHJnbnQrAqxiiJv4ADi_L76d5_T8_1z0Ea_ZO7h2I-35JHOu43bflHEbj-G5kzFRsyhBhkJZwHc8",
                         "Andrei Gorbunkov"
-                },*/
+                },
+
                 {       "paul-bereza",
                         "paul.bereza02@outlook.de",
                         "33222200Shin",
-                        "AQEDAUt7kjAFQ5V1AAABjY17BIQAAAGOrchRxk0ANnqFXzwoOoGvqdSns-mBZgVIAigOKbTZJ0RWUBNK8UB64oOwoeTXFih1uxrDgIW8DA6YEpGKavO-W1MdA2YdEUSLqalw5LgoK7VQMCDwi7SmvsPn",
+                        "AQEFAREBAAAAAA9y_ngAAAGPTWyIZQAAAY9xerrsTQAAtHVybjpsaTplbnRlcnByaXNlQXV0aFRva2VuOmVKeGpaQUFDbnRYRkMwRzBrTkJ0UnhBdGNPak1TMFlRSS9uZXNWNHdJN0krWUM4REl3RE1hUW9RXnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjIxMjU2Mjg0OSwzMDMyMjU2NjUpXnVybjpsaTptZW1iZXI6MTI2NjM4OTU1Mqtahj54xFZSxnrvy1Fhjo_T5CKqWfcLzzEpATi94tKyJSkHYCbgDxcQJJBeH2a8gJu17Nee6uDtPtfo_xlPThNbQRG8NrY1MvzU75pvL6Rvd7fsGhGb9M58udN77XZ1qFysniogLshJfVj7ldODM6ZXSGdf68Gml-blsKo3mCQX9GbJCdGFEc0IjGUj57r7S4gaOng",
                         "Paul Bereza"
                 },
                 {       "alessio-vacenko-b506612b3",
                         "alessio.Vacenko@outlook.it",
                         "33222200Shin",
-                        "AQEDAUthqywBUW83AAABjoGzPasAAAGOpb_Bq00AYesxSzqz_svmdxwpgeBI81kGKBz9KOqrYeQvGGre8kwdFnnUm2mcCEmofBpk5hydytWEP2hVdpRs910CvZ5kko7h1JcCY1jsGoAdpRxNvdqaCuac",
+                        "AQEDAUthqywCOthiAAABj00h33QAAAGPvkp4OE4ASeyim2hF4oGJkCfAbCkjDWmIdJfwVO3XyI1HeaM_0-kmXSiiMnwHo7xnp6urbl5O3VczT6kuWjuBlXHiAKh7-TSsVSn6Vrc2y521ONn_sVF2klqU",
                         "Alessio Vacenko"
                 },
                 {       "margit-matthes",
                         "margit.Matthes@outlook.de",
                         "33222200Shin",
-                        "AQEDAUuampkCi39pAAABjoGopG0AAAGOpbUobU0AYzgDBXlGuCNM8mjS3qeymKlCs3GDAAAX2yazREUmzNsgqrkCEBZWhzFf7xDJ05XaRilGTXcvDKyDYGYfmp7b-E2i0L4QTVBbKA7d6GA6MsD6y_Hb",
+                        "AQEDAUuampkDA9uSAAABjzNCPogAAAGPV07CiFYAUTlaawCUQBq15anEMNwZmKlCqaTK2oqUSr-P05fihZvoGHPgFk7KZroX9ZKpVEbvEjOLth1xcqkJDG3F_a2_o_3WYwet4mooFR5SjuTy2Z_eezlj",
                         "Margit Matthes"
                 },
-
-
                 {       "dmitriy-timashov",
                         "timashov.dmitriy@outlook.de",
                         "33222200Shin",
-                        "AQEDAUsszhwEAKkFAAABjWPddXUAAAGO5z0PXE0ARMhQR0-MOEBRh7EyBGRM-S4jmoyUlHfdOlcjgh6xVE_e2dvxPYueFIGTfc1OUqDu-sfytCMrY7SpLdhYUKsjMrrlIjul4LmJu2_DWUNg5JkJ4usr",
+                        "AQEDAUsszhwD-UTVAAABjzNJL1kAAAGPV1WzWU0Am_0hKUQRi5bvt9ZVU15-Yig0zqKpUaul-TQl69vjsUu4AX_e5zQ9o0HR6yR3IQZpg56KgFMtjg6JC54K00TwLxRDc-_GMW5K4ECEUrrX8BVS6nZ7",
                         "Dmitriy Timashov"
                 },
 
-
-                {       "Eliza Kolner",
+                {       "eliza-kolner",
                         "eliza.kolner0103@outlook.de",
                         "ek03303KK",
-                        "AQEDAUxQ-4UD3vPhAAABjxTuV1IAAAGPOPrbUk0AvEPC5Cd50KZiOxdo3qTYvsLG313MO07nNEIDgQhwKnVf-1jaGp30OAx7CfWnRBm2xte8pFHe6qT3mimH6Mwh21hwiZUUgV-q6f80Up7yDy-M1a5P",
+                        "AQEFAREBAAAAAA9zBMEAAAGPTWfEpAAAAY_lKiqTTQAAtHVybjpsaTplbnRlcnByaXNlQXV0aFRva2VuOmVKeGpaQUFDbnRYRkUwRzBrTkNMbXlCYTRIRGhTa1lRSS9uZVhsMHdJN0krNVFZREl3RExpQW5SXnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjIxMjU2MjgzMywzMDMyMjkxNDUpXnVybjpsaTptZW1iZXI6MTI4MDM3NTY4NWp8bEsKJ75ogALhXQXQOlD3_brt0oR5mr_80U-ICsdrFqy-1_NJDjBInsNdgTVTTTiucKXT3yLe7uO0ozSA-OpT2CoPk-eUXF8diJIz_3Z_ciTBjWjlYNDM16fYlOI0mnZ_P-RdJDWNMvtQwywWon_TNUL5nyhQROKH4yOtj5VTz4BbSV5zMWjs405mZxzJEzUYwds",
                         "Eliza Kolner"
                 },
 
@@ -164,7 +169,34 @@ public class ChangeLead {
                         "petr.degtyarev@outlook.de",
                         "33222200Shin",
                         "AQEDAUs5cmoFe9mZAAABjlyBf2oAAAGPMzVXok0Aase-74YwTB2el__HXiApMk1VKjDZtiYnIDeGI_Ez0PCLecWzCQhotwuxGmOsGFdTDsfLoyvCiOiFkdEKpoiqtjPBEqoZR_aSsiFM2OhDXtMNs2cp",
-                        "petr-2"
+                        "Petr Degtyarev"
+                },
+
+                {       "elias-danilov",
+                        "elias.danilov@outlook.it",
+                        "33222200Shin",
+                        "AQEDAUs6XDsD5xZNAAABj9280AUAAAGQAclUBU0AVLD-KLt7E5u2l9vaxLlyJsy4IwByMkkffUZHu29ve_BIdisK6fCkVzQntizDPNKr2zztLhhvP6qpY79-cfeghLQlRQ66Ahhj3QM3WxroRP9rwk-d",
+                        "Elias Danilov"
+                },
+
+                {       "stefania-mykhaylenko",
+                        "mykhaylenko.stefania@outlook.fr",
+                        "cTsH3KhU",
+                        "AQEDAUxQ7yQCBDVPAAABj86bRwQAAAGP8qfLBE0Ax1VldZxwdUXUKLTFU60qzlcZGvAR2RZG_IkS8ifmuXHdFVYU8Q5I4wPPo9yyB9qNP39-xvU7pluH2AB6hTGmmp9kzRBbTXUoKEVi0mAZ8D2cLJjx",
+                        "Mykhaylenko Stefania"
+                },
+
+                {       "eliza-kolner",
+                        "eliza.kolner0103@outlook.de",
+                        "ek03303KK",
+                        "AQEFAREBAAAAAA9zBMEAAAGPTWfEpAAAAY_lKiqTTQAAtHVybjpsaTplbnRlcnByaXNlQXV0aFRva2VuOmVKeGpaQUFDbnRYRkUwRzBrTkNMbXlCYTRIRGhTa1lRSS9uZVhsMHdJN0krNVFZREl3RExpQW5SXnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjIxMjU2MjgzMywzMDMyMjkxNDUpXnVybjpsaTptZW1iZXI6MTI4MDM3NTY4NWp8bEsKJ75ogALhXQXQOlD3_brt0oR5mr_80U-ICsdrFqy-1_NJDjBInsNdgTVTTTiucKXT3yLe7uO0ozSA-OpT2CoPk-eUXF8diJIz_3Z_ciTBjWjlYNDM16fYlOI0mnZ_P-RdJDWNMvtQwywWon_TNUL5nyhQROKH4yOtj5VTz4BbSV5zMWjs405mZxzJEzUYwds",
+                        "Eliza Kolner"
+                },
+                {       "andrey-gorichev",
+                        "gorichev.andrey12311@outlook.it",
+                        "33222200Shin",
+                        "AQEDAUsXduoFXURrAAABjVq7ABQAAAGP47hNrE0AfWZ8_ikQZ884R2ykvvKS1bOA0bUDRo0-6r0_QFmBrzWZvqxX_JCmB-X5RykYyrSh6LzajqQcrohQA8pmVb7XqF8MMY24l9X2v4tQfU3OeyOnIpyd",
+                        "Gorichev Andrey"
                 },
         };
     }
