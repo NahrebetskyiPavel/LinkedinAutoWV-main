06b5a496327753154d77afc990197c43b3a4fa53
Hryhorii Opanasenko <pnagrebetskyi@gmail.com>
2024-11-15 16:14:12 +0200

    fix

--- src/test/java/com/automation/linkedin/AddLeads.java
+++ src/test/java/com/automation/linkedin/AddLeads.java
@@ -248,20 +248,7 @@ public class AddLeads extends Base {
                         accountInfos.get(9).get(0),
                 },
                 //11
-                {
-                        accountInfos.get(10).get(0),
-                        accountInfos.get(10).get(1),
-                        accountInfos.get(10).get(2),
-                        "Hi. I stumbled upon your account and noticed that you have expertise in my area of interest. I was wondering if you would mind having a chat about the real estate market in the US, its challenges and opportunities ;)",
-                        accountInfos.get(10).get(0),
-                },        //11
-                {
-                        accountInfos.get(11).get(0),
-                        accountInfos.get(11).get(1),
-                        accountInfos.get(11).get(2),
-                        "Hi. I stumbled upon your account and noticed that you have expertise in my area of interest. I was wondering if you would mind having a chat about the real estate market in the US, its challenges and opportunities ;)",
-                        accountInfos.get(11).get(0),
-                },
+
 
 
 
