2e261e4271b96fba9957cdff9a7dc3d89e0e15e8
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2025-03-19 14:08:27 +0200

    fox

--- src/test/java/com/automation/linkedin/Message.java
+++ src/test/java/com/automation/linkedin/Message.java
@@ -38,7 +38,7 @@ public class Message extends Base{
     int msgsSent = 0;
     ArrayList<Integer> taskIdList = new ArrayList<>();
     int msgsSentCounter = 0;
-    int msgsSentCounterMax = 40;
+    int msgsSentCounterMax = 20;
 
 
     @SneakyThrows
