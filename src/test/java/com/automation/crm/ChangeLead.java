44e4007fa0dcf1aa95447b0421ab9beb77a2800c
pro16 <pro16@MacBook-Pro.local>
2025-03-31 15:51:11 +0300

    adaad

--- src/test/java/com/automation/crm/ChangeLead.java
+++ src/test/java/com/automation/crm/ChangeLead.java
@@ -109,35 +109,22 @@ while (true){
                 {       "Evgeny-Gazitov",
                         "evgeny.gazitov8753@outlook.it",
                         "33222200Shin",
-                        "AQEDAUsJgSoACPECAAABlX91PhYAAAGVo4HCFlYARxppAFEu2z2PuxjuXYBO7OBLatNyhy84r3VkAanlhTBQyIvD4vbvLbIG1mhiiw7EpTXvuy0eNGNU9nGxGP-_rQqEi5lbfoQQper6afSjzZeQEfIQ|eyJsYW5ndWFnZXMiOlsiaXQtSVQiLCJpdCIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJpdC1JVCIsInRpbWVab25lIjoiRXVyb3BlL1JvbWUiLCJpcCI6Ijk1LjI0MC4yMDYuMTk0In0=",
+                        "AQEDAUsJgSoE5ofAAAABlcebCyQAAAGWD_4A0lYAkB6R2FVcvduIaIcV16UnW4wHWOVSBRxv0P2VEcFTmRk5JIpbIo0Surd7dls9lfrHRf8-m-EBehV2sC63FxTtZhaOA21qDSmKEh6-Ieil3t3XXpEx|eyJsYW5ndWFnZXMiOlsiaXQtSVQiLCJpdCIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJpdC1JVCIsInRpbWVab25lIjoiRXVyb3BlL1JvbWUiLCJpcCI6IjIuMTk2LjE0My44OCJ9",
                         "Evgeny Gazitov"
                 },
 
                 {       "Johan-Heinlein",
                         "johan.heinlein@outlook.de",
                         "eGdFPRgS",
-                        "AQEDAUxEX5oApf6pAAABjglKvk4AAAGU37pv3E0Aj86fZrt57nf9dDQ9L2ycAOFb1RU57UHtsDbugqCEgQI9RWHVyVFlgLNiv9OrCA8Ljw3_6SwfcVDdvLa4sBTiEznok-P7bnKgFpD9l5u9N5ilTHF_|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiMzcuMjAxLjE5OS4yMSJ9",
+                        "AQEDAUxEX5oApf6pAAABjglKvk4AAAGWEAJwmU0AyB2gR12uCPMppNnZ6LDo2GN8V_Zlw-kKZ8oP8UBS0liNlTAPXoBTrkag5znDFEIKQMrUMckrvBSCDLtB0dxoi1LmWRjI4ZHmwmRmq21djZUOLI6K|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODcuMTg3LjUzLjI0NSJ9",
                         "Johan Heinlein"
-                },
-                {       "Artemio-Chumakov",
-                        "artemio.chumakov1981@outlook.it",
-                        "33222200Shin",
-                        "AQEDAUtCHbcD5C-bAAABkcaWbF4AAAGU37dZ7E0AvbdmKlhCZSBkmNh-AjzXAruOXXZvwJkbpduIywU_XK8rQ9OA7HvxFIWRKos9zQdvCNqVWfhMTSR_HsNJoGlt2gECiowvI-wx55iz52P1Qvy4Z0Tz|eyJsYW5ndWFnZXMiOlsiaXQtSVQiLCJpdCIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJpdC1JVCIsInRpbWVab25lIjoiRXVyb3BlL1JvbWUiLCJpcCI6bnVsbH0=",
-                        "Artemio Chumakov"
-                },
-
-                {       "patrick-yushko-b2080b2b8",
-                        "yushko.patrick@outlook.it",
-                        "206GLMC2",
-                        "AQEFAHUBAAAAABBl9t0AAAGPfE7YuwAAAZMaRBSVVgAAGHVybjpsaTptZW1iZXI6MTI4MDAxMjQyNMyB0U3N4EXZoT7jRSgrh2ZsRQhw3dIPIjxxs7HK2bI8jIXKrSaNXE-7GhbppvBOQSO2mokFi0nLkNu11TQ3PPReQB8-2boUF0iWaZ7L3W1dFU9X07glDGQPpLsaMRPWQju-eZbs2y2zeE1w8P2PvROcYJDwbJXaXTTxwbFor9oT7iUhsfAU30z5UF7qX0VUHdnorZw",
-                        "Yushko Patrick"
                 },
 
 
                 {       "michael-krusciov",
                         "michael.krusciov@outlook.de",
                         "cTsH3KhU",
-                        "AQEDAUwy4cUDY3T1AAABlX9lJx8AAAGVo3GrH00AKYznEfusBysjPG529_HO2FaM1rf2Ua3DKqRXYTGS-ToJDCCXB2uqjFMIjTqjO_iHygqxb3sjwA0RSj6E8wO7GDTSGmEBWVrxyvbUPry7PJKavlV0|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiMTg1LjE4My4xNzkuNiJ9",
+                        "AQEDAUwy4cUCeFSdAAABlceaHS8AAAGWEAjilE0AdQuD_Kf9jbbJlvTzCf3xvQUAVbL79_bVUxAuH6m3vbREk6uei1EDvoKNqoCsdKe4l1iM2glapAxFfqax1q3c9WAOQBusMxT-y4KgQxjYzK_VTq6y|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODcuMTQ4Ljk0LjI1In0=",
                         "Michael Krusciov"
                 },
 
@@ -202,8 +189,34 @@ while (true){
                         "metmar11mmjy",
                         "AQEDAUvkzscEuod7AAABjcxZHVEAAAGVZaJUXk0AZtEe_NDTtXzWjNSIhGHwaK6uxLrfIID9n94Trrah5UBV8qZbnsjj0uRerARQuFR5AHFRd3uAGQFE5uMGzGZIzZNmYbG90TRwHOjFjCHsFnQDGI4f|eyJsYW5ndWFnZXMiOlsiaXQtSVQiLCJpdCIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJpdC1JVCIsInRpbWVab25lIjoiRXVyb3BlL1JvbWUiLCJpcCI6Ijg0LjMzLjI1MS4xNzgifQ==",
                         "Matthew Martinez"
-                }
+                },
 
+                {       "Danylo-Lytvyn",
+                        "wisevision.beast@gmail.com",
+                        "1171534Oli35Wisew",
+                        "AQEDATP_TCAAuywlAAABldbcruUAAAGV-uky5U0AkM0fAZ1ACH_RNZBXhZfRda3UIsoXgC6uBBNnKaUq5-_x_s9QU73z7DnspGpAfdYX3IrLk0Lpc6NdoYBWuqNi8bKHBLM9lxQSBuy-gMbiAb2IB29C|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiMTg4LjI0NS4xOTkuMjA1In0=",
+                        "Danylo Lytvyn"
+                },
+                {       "Tobias-Rogers",
+                        "tobias.Roger22@outlook.de",
+                        "tobi0022",
+                        "AQEDAUvEkSUFt1TnAAABjbzQTxkAAAGWEEZdlU0ArTIedk8SiSfeZz1wwkzdJApWu7oCkfZU_t_Tpa9mp63o6d3u2QcdPvsFEOe2evNcE_jGvIL8cYZcnvumgC5x9f1NUTcZdw3mlSVdbtZv1V25ptuK|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODcuMTg3LjUzLjI0NSJ9",
+                        "Tobias Rogers"
+                },
+
+                {       "Elias-Danilov",
+                        "elias.danilov@outlook.it",
+                        "33222200Shin",
+                        "AQEDAUs6XDsDjEpoAAABlNtvWv4AAAGWEEoPxFYAlxhiqAFDf6xAeERQcffj_FvXqxOJKlTNroEMyRlQObH9YlZBg34S3QdmgYtnAZO0PKefBihAywBfk8660VFcc3gofbT6rPfzC28eZcYh2bP181zl|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODcuMTg3LjUzLjI0NSJ9",
+                        "Elias-Danilov"
+                },
+
+                {       "Paul-Bereza",
+                        "paul.bereza02@outlook.de",
+                        "33222200Shin",
+                        "AQEFAHUBAAAAAA9y_ngAAAGQEc-OggAAAZYQTN67TQAAGHVybjpsaTptZW1iZXI6MTI2NjM4OTU1Mjw40NKny46BecwpVQacg8DF9eKq4db6iS_Z29krNiJFviZxwZBQiBHsAnxNYEMIvVgWcyV5ZOmLSI4BQmrZnrUCWjsq1o6xvg7fPQcKrWfFi_ulYrcMVKLRuQNkbfhA0QelpQiLzobsxKEavez7xRoXbkuUKBZx0ChnA6zDySjyfTgHXK-0sYeNAZMqjCIUT2dpAJs|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODcuMTg3LjUzLjI0NSJ9",
+                        "Paul Bereza"
+                }
         };
     }
 }
