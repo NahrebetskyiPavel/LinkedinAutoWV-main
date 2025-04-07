127230fb6e33cb8cc7b6f023f301fb7b4c64cd75
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2025-04-07 18:57:03 +0300

    fox

--- src/test/java/com/automation/linkedin/Message.java
+++ src/test/java/com/automation/linkedin/Message.java
@@ -410,7 +410,7 @@ public class Message extends Base{
                 {       "michael-krusciov",
                         "michael.krusciov@outlook.de",
                         "cTsH3KhU",
-                        "AQEDAUwy4cUD2rnjAAABlgyITGcAAAGWMJTQZ00Ax-xuBYJ5WpiUfTGiXNTiQc4sPdgFHqmdqVJd54AMGAGwDuNt1JzsIGKHqDZ3hJvC0u0VNdxLzxRPCxI_bT_zjfNO54aXMEUwKGRJwz6XfL2qo8NQ|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiMjE3LjIyNy4xMzguMTg2In0=",
+                        "AQEDAUwy4cUD2rnjAAABlgyITGcAAAGWMJTQZ00Ax-xuBYJ5WpiUfTGiXNTiQc4sPdgFHqmdqVJd54AMGAGwDuNt1JzsIGKHqDZ3hJvC0u0VNdxLzxRPCxI_bT_zjfNO54aXMEUwKGRJwz6XfL2qo8NQ|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiNzcuMTc3LjI5LjYxIn0=",
                         "Michael Krusciov"
                 },
 
@@ -483,17 +483,13 @@ public class Message extends Base{
                         "AQEDATP_TCAAuywlAAABldbcruUAAAGV-uky5U0AkM0fAZ1ACH_RNZBXhZfRda3UIsoXgC6uBBNnKaUq5-_x_s9QU73z7DnspGpAfdYX3IrLk0Lpc6NdoYBWuqNi8bKHBLM9lxQSBuy-gMbiAb2IB29C|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiMTg4LjI0NS4xOTkuMjA1In0=",
                         "Danylo Lytvyn"
                 },
-                {       "Tobias-Rogers",
-                        "tobias.Roger22@outlook.de",
-                        "tobi0022",
-                        "AQEDAUvEkSUFt1TnAAABjbzQTxkAAAGWEEZdlU0ArTIedk8SiSfeZz1wwkzdJApWu7oCkfZU_t_Tpa9mp63o6d3u2QcdPvsFEOe2evNcE_jGvIL8cYZcnvumgC5x9f1NUTcZdw3mlSVdbtZv1V25ptuK|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODcuMTg3LjUzLjI0NSJ9",
-                        "Tobias Rogers"
-                },
+
+
 
                 {       "Elias-Danilov",
                         "elias.danilov@outlook.it",
                         "33222200Shin",
-                        "AQEDAUs6XDsDBqcbAAABlfrHGTQAAAGWHtOdNE0ABBd1EGGnxYv49gzS8GoGYirsBxNgw9Pc32bpm1kzBnndLVPOfC9O3rdav_240v8RAla45jbUGlC9-nW7yMwxGnM5eTDDzG5_m801npN_oEvCmxLA|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiOTMuMTMxLjE5NC4xNDgifQ==",
+                        "AQEDAUs6XDsBm6RTAAABlg9rOyMAAAGWM3e_I00A0Bi1K4wkBT4oLlJ1osDHS1138nYD6D2uIFDJdXS_rozzry4_-KOUgFytoJgUMHp-E-zdzS7MhuI7nFv_igKg0MWnCt_7ZsUlmC81sYxhU2OKQMMa|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODQuMTUwLjE2MS4xNSJ9",
                         "Elias Danilov"
                 },
 
