dc6f131d8d06b6bdc63f5ab51bd4b118df4bb893
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2025-04-07 18:57:50 +0300

    fix

--- src/test/java/com/automation/linkedin/AddLeads.java
+++ src/test/java/com/automation/linkedin/AddLeads.java
@@ -281,7 +281,7 @@ public class AddLeads extends Base {
                 {       "michael-krusciov",
                         "michael.krusciov@outlook.de",
                         "cTsH3KhU",
-                        "AQEDAUwy4cUAQXK2AAABlfWFaxAAAAGWGZHvEFYAZxuhYbbxFew9rOO68Mt7TjLmpU0-BQjDUGKbU-ZBrL9jVw2wuEkBZHKqmTXBOB717Qr0AV7X0eXM7cxi24yReeJjqfiuJ8k8ajYhoTKYhvi2QO4f|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiOTEuMC44OS42OCJ9",
+                        "AQEDAUwy4cUD2rnjAAABlgyITGcAAAGWMJTQZ00Ax-xuBYJ5WpiUfTGiXNTiQc4sPdgFHqmdqVJd54AMGAGwDuNt1JzsIGKHqDZ3hJvC0u0VNdxLzxRPCxI_bT_zjfNO54aXMEUwKGRJwz6XfL2qo8NQ|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiNzcuMTc3LjI5LjYxIn0=",
                         "Michael Krusciov"
                 },
 
@@ -291,13 +291,13 @@ public class AddLeads extends Base {
                         "asd2424qq",
                         "AQEDATp4HzoBa6O7AAABlGlxPOgAAAGUjX3A6E0AwNpK6n3p3Ulp5DO-BwvtTrsYEBYy9LxAD6xohwUCfLQO52TrKY4CuGewUPx04ho-yh9X6s0CZ_L_uBtMC0MvER8xlAxfO5ENZX--bzIUQuvtufWI|eyJsYW5ndWFnZXMiOlsidWstVUEiLCJ1ayIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJ1ay1VQSIsInRpbWVab25lIjoiRXVyb3BlL0tpZXYiLCJpcCI6IjE4OC4xNjMuMjYuNjkifQ==",
                         "Anastasiia Vozniak"
-                },
+                },/*
                 {       "Art-Stenko",
                         "artstenko@gmail.com",
                         "GOgoCyclone_11",
                         "AQEDAR1-TWgCdaXbAAABlGmXZtsAAAGU71o8iU0AX2vCEj5S0ms2kzRxYBRPWlb-BlkleFw59RilRgTGUZKEKeA3LHcXC5v2PEGQ4htj0m20Zgs8vTv6FxAjj6ZwLwCao7rehPhPpbiaXZbxwkBFGHGs|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiMTg4LjI0NS4xOTkuMjA1In0=",
                         "Art Stenko"
-                },
+                },*/
                 {       "lina-Kompanets",
                         "ekompanets02@gmail.com",
                         "35ulurev",
@@ -354,17 +354,13 @@ public class AddLeads extends Base {
                         "AQEDATP_TCAAuywlAAABldbcruUAAAGV-uky5U0AkM0fAZ1ACH_RNZBXhZfRda3UIsoXgC6uBBNnKaUq5-_x_s9QU73z7DnspGpAfdYX3IrLk0Lpc6NdoYBWuqNi8bKHBLM9lxQSBuy-gMbiAb2IB29C|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiMTg4LjI0NS4xOTkuMjA1In0=",
                         "Danylo Lytvyn"
                 },
-                {       "Tobias-Rogers",
-                        "tobias.Roger22@outlook.de",
-                        "tobi0022",
-                        "AQEDAUvEkSUFt1TnAAABjbzQTxkAAAGWEEZdlU0ArTIedk8SiSfeZz1wwkzdJApWu7oCkfZU_t_Tpa9mp63o6d3u2QcdPvsFEOe2evNcE_jGvIL8cYZcnvumgC5x9f1NUTcZdw3mlSVdbtZv1V25ptuK|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODcuMTg3LjUzLjI0NSJ9",
-                        "Tobias Rogers"
-                },
+
+
 
                 {       "Elias-Danilov",
                         "elias.danilov@outlook.it",
                         "33222200Shin",
-                        "AQEDAUs6XDsDBqcbAAABlfrHGTQAAAGWHtOdNE0ABBd1EGGnxYv49gzS8GoGYirsBxNgw9Pc32bpm1kzBnndLVPOfC9O3rdav_240v8RAla45jbUGlC9-nW7yMwxGnM5eTDDzG5_m801npN_oEvCmxLA|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiOTMuMTMxLjE5NC4xNDgifQ==",
+                        "AQEDAUs6XDsBm6RTAAABlg9rOyMAAAGWM3e_I00A0Bi1K4wkBT4oLlJ1osDHS1138nYD6D2uIFDJdXS_rozzry4_-KOUgFytoJgUMHp-E-zdzS7MhuI7nFv_igKg0MWnCt_7ZsUlmC81sYxhU2OKQMMa|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiODQuMTUwLjE2MS4xNSJ9",
                         "Elias Danilov"
                 },
 
