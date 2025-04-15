cfb1d777229b4db086511cefd94e3e50788e0119
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2025-04-15 18:12:41 +0300

    fix

--- src/test/java/com/automation/linkedin/Message.java
+++ src/test/java/com/automation/linkedin/Message.java
@@ -97,6 +97,7 @@ public class Message extends Base{
         };
 
         for (int n = 0; n < 1000; n++) {
+            if (msgsSentCounterMax > 20) break;
             String data =  zoho.getLeadList(token, "Contacted", linkedinAccount, n);
             if (data.contains("INVALID_TOKEN")) {
                 token = zoho.renewAccessToken();
