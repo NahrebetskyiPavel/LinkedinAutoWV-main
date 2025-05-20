0bdef332950bb1948e5e6333322d3de78c3650f7
pro16 <pro16@MacBook-Pro.local>
2025-05-20 15:23:55 +0300

    adaad

--- src/test/java/com/automation/linkedin/Message.java
+++ src/test/java/com/automation/linkedin/Message.java
@@ -398,7 +398,7 @@ public class Message extends Base{
     @DataProvider(name = "dataProviderPeopleSearch", parallel=true)
     public static Object[][] dataProviderPeopleSearch() {
         return new Object[][]{
-/*
+
 
                 {       "Johan-Heinlein",
                         "johan.heinlein@outlook.de",
@@ -406,7 +406,7 @@ public class Message extends Base{
                         "AQEDAUxEX5oClGSZAAABlqRwr-QAAAGWyH0z5E0AXqUVg3i_xGUGHfkK0521QgvnRT3oGDKSMjzCJj7WtNcFtZLzXvtO1iSWVYZeijqfm8HQAoM35AbLaxh8ZqniHsPPrte8YtkkgtXT5XfPBbp_q4YO|eyJsYW5ndWFnZXMiOlsiZGUtREUiLCJkZSIsImVuLVVTIiwiZW4iXSwibGFuZ3VhZ2UiOiJkZS1ERSIsInRpbWVab25lIjoiRXVyb3BlL0JlcmxpbiIsImlwIjoiMTU2LjIyOC4xNzEuMjUifQ==",
                         "Johan Heinlein"
                 },
-*/
+
 
                 {       "Anastasiia-Vozniak",
                         "vozniakanastasia52@gmail.com",
