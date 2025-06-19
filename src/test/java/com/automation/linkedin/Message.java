0feef2f500312851a753327b6a0a93bf86517831
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2025-06-19 07:43:23 +0300

    fix

--- src/test/java/com/automation/linkedin/Message.java
+++ src/test/java/com/automation/linkedin/Message.java
@@ -103,7 +103,7 @@ public class Message extends Base{
         };
 
         for (int n = 0; n < 1000; n++) {
-            if (msgsSentCounterMax > leadsRandomResult) throw new Exception("msg limit" + linkedinAccount);;
+            if (msgsSentCounterMax < leadsRandomResult) throw new Exception("msg limit" + linkedinAccount);;
             String data =  zoho.getLeadList(token, "Contacted", linkedinAccount, n);
             if (data.contains("INVALID_TOKEN")) {
                 token = zoho.renewAccessToken();
