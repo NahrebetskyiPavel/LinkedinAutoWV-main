23cf492db39f40253961180b516156d84235172b
pro16 <pro16@MacBook-Pro.local>
2025-06-19 12:39:18 +0300

    adaad

--- src/test/java/com/automation/linkedin/Message.java
+++ src/test/java/com/automation/linkedin/Message.java
@@ -38,7 +38,7 @@ public class Message extends Base{
     int msgsSent = 0;
     ArrayList<Integer> taskIdList = new ArrayList<>();
     int msgsSentCounter = 0;
-    int msgsSentCounterMax = 2;
+    int msgsSentCounterMax = 30;
 
     @BeforeMethod
     void clearCount(Method method){
