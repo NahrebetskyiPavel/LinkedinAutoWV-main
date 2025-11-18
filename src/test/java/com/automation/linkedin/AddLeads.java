60216f07d01f46ee1beb8e08ca1ef085dad60b85
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2025-11-18 12:32:34 +0200

    fix

--- src/test/java/com/automation/linkedin/AddLeads.java
+++ src/test/java/com/automation/linkedin/AddLeads.java
@@ -334,13 +334,13 @@ public class AddLeads extends Base {
                         "nmogilna77@gmail.com",
                         "ywZ9YRc-,!k6P*-",
                         "",
-                        "Anastasiia-Mohylna"
+                        "Anastasiia Mohylna RD"
                 },
                 {       "Yura-Demchuk",
                         "yura.demchukk@gmail.com",
                         "&%2jt-68PqeJ5,S",
                         "",
-                        "Yura-Demchuk"
+                        "Yura Demchuk RD"
                 },
 
                 /*          {       "Danylo-Lytvyn",
